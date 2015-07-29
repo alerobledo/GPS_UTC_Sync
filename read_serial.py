8c6f0b69b11d004f38f4c02a3f132c6d7f2febec
alerobledo <alerobledo@gmail.com>
2015-07-29 17:02:33 -0300

    create 2 differents process to read in parallel both serial port

--- read_serial.py
+++ read_serial.py
@@ -1,9 +1,25 @@
 #example call this script:  python read_serial.py /dev/ttyACM0
 
+import multiprocessing
 import serial
 import time
 import sys
 
-ser = serial.Serial(sys.argv[1], 115200)
-while True:
-	print 'Value received: ' + repr(ser.readline()) +'   - at time: ' + repr(time.time())
+def readSerial(serialPort, baudRate):
+        ser = serial.Serial(serialPort, baudRate)
+        while True:
+                print 'Value received: ' + repr(ser.readline()) +'   - at time: ' + repr(time.time())
+
+#sys.argv[1]
+                
+if __name__ == "__main__":
+    hilo1 = multiprocessing.Process(target=readSerial, args=("COMM1", 115200))
+    hilo2 = multiprocessing.Process(target=readSerial, args=("COM2", 115200))
+
+    print "Launching threads"
+    hilo1.start()
+    hilo2.start()
+    hilo1.join()
+    hilo2.join()
+    print "Done"
+
